d52ad7554fdcd4d6627298e8bc911876cf84953b
PriteeTangle <140731865+priteetangle1984@users.noreply.github.com>
2024-04-25 13:17:25 -0400

    deploying

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,10 +1,8 @@
-import { useState } from 'react'
+import { useState } from "react";
 
-function TodoItem({todo, removeItem, toggleComplete, editItem}) {
-
+function TodoItem({ todo, removeItem, toggleComplete, editItem }) {
   const [editing, setEditing] = useState(false);
-  const [editText, setEditText] = useState(todo.text);  
-
+  const [editText, setEditText] = useState(todo.text);
 
   function handleEdit() {
     setEditing(true);
@@ -16,35 +14,49 @@ function TodoItem({todo, removeItem, toggleComplete, editItem}) {
 
   function handleSave() {
     setEditing(false);
-    editItem(todo, editText);   // passing the todo object
+    editItem(todo, editText); // passing the todo object
   }
 
   function handleTextChange(e) {
-    setEditText(e.target.value)
+    setEditText(e.target.value);
   }
-
 
   return (
     // TodoItem
     <li>
-      {editing ? (<input
-          type="text" value={editText} onChange={handleTextChange} />) : (
-            <div>
-              <input
-                type="checkbox" 
-                checked={todo.completed}     
-                onChange={() => toggleComplete(todo.text)} />
-                <span style={{ textDecoration: todo.completed ? 'line-through' : 'none' }}>
-                {todo.text}
-                </span>              
-            </div>
-          )}
-          <button onClick={handleEdit} disabled={editing} className='editBtn'>Edit</button>
-          <button onClick={handleRemove} disabled={!todo.completed} className='delBtn'>Delete</button>
-          {editing && 
-            <button onClick={handleSave} className='saveBtn'>Save</button>}
+      {editing ? (
+        <input type="text" value={editText} onChange={handleTextChange} />
+      ) : (
+        <div>
+          <input
+            type="checkbox"
+            checked={todo.completed}
+            onChange={() => toggleComplete(todo.text)}
+          />
+          <span
+            style={{ textDecoration: todo.completed ? "line-through" : "none" }}
+          >
+            {todo.text}
+          </span>
+        </div>
+      )}
+      <button onClick={handleEdit} disabled={editing} className="editBtn">
+        Edit
+      </button>
+      <button
+        onClick={handleRemove}
+        disabled={!todo.completed}
+        className="delBtn"
+      >
+        Delete
+      </button>
+      {editing && (
+        <button onClick={handleSave} className="saveBtn">
+          Save
+        </button>
+      )}
     </li>
   );
 }
 
-export default TodoItem
+export default TodoItem;
